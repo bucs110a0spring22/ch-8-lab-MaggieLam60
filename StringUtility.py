aeb9bfcbdb68c26b25ab940b8a57f5fdc36b79a1
Maggie Lam <98533737+MaggieLam60@users.noreply.github.com>
2022-04-13 02:42:51 +0000

    code

---
+++ StringUtility.py
@@ -0,0 +1,97 @@
+#Part A
+class StringUtility:
+  
+  
+  def __init__(self, string):
+    self.string=string
+  '''
+  general function description= is the init function
+	args: self and string
+	return: nothing, init
+  
+  '''
+  def __str__(self):
+    return self.string
+  '''
+  general function description= is the str function
+	args: self 
+	return: self.string
+  
+  '''
+    
+#Part B
+
+  def vowels(self):
+    '''
+    general function description= returns the # of vowels less than 5
+	args: (type) description= self
+	return: (type) description= returns many and # of vowels
+    '''
+    vowels= ['a','e','o','i','u']
+    number_of_vowels = 0
+    for character in self.string:
+      if character in vowels:
+        number_of_vowels += 1  
+    if number_of_vowels >= 5:
+        return ("many")
+    else: 
+        return str(number_of_vowels)
+  
+  def bothEnds(self):
+    '''
+    general function description= adds the first and last 2 letters
+	args: (type) description= self
+	return: (type) description= returns '' and first and last 2 letters of word
+    
+    '''
+    if len(self.string) <= 2:
+      return "" 
+    else:
+      return self.string[0] + self.string[1] + self.string[-2] + self.string[-1]
+        
+  def fixStart(self):
+    '''
+    general function description= replaces char of 1st letter with *
+	args: (type) description: self 
+	return: (type) description: new string or self if less than 1
+    '''
+    if len(self.string) <= 1:
+      return self.string
+      
+    firstletter = self.string[0]
+    new_string = firstletter 
+    
+    for index in range(1,len(self.string)):
+      if self.string[index] == firstletter:
+        new_string+= '*'
+      else:
+        new_string += self.string[index]
+        
+    return new_string
+
+  def asciiSum(self):
+    '''
+    general function description= adds the sum of the char
+	args: (type) description= self
+	return: (type) description= returns the sum of the char
+    '''
+    sum = 0
+    for char in self.string:
+      sum += ord(char)
+    return sum
+    
+  def cipher(self):
+   length = len(self.string)
+   new_string = ''
+   for char in self.string:
+     if char.isalpha():
+       new_string += chr(ord(char)+length)
+     else:
+       new_string += char
+   return new_string
+   '''
+  general function description= changes the letter in alphebetical order by the number of total 
+	args: (type) description= self
+	return: (type) description = returns new string made
+'''
+  
